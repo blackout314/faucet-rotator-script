9961af3a240d7449c6a13d3b4db05e976eab19b1
blackout314 <blackout_lair@yahoo.com>
2015-11-11 18:05:00 +0100

    import from P1R4T3 work :camel:

---
+++ ro.js
@@ -0,0 +1,93 @@
+var s=[
+"2.html",
+"http://freesatoshi.info/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://www.lovebitcoin.eu/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://zoobit.co.in/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://rekbitcoin.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://faucetsatoshi.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://claimbit.co.uk/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://fastbtc.xyz/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://minions-faucet.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://btc-snack.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://www.pickupcoin.com/btc/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://greenbitco.in/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://wsxgames.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://www.valley-bitcoin.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://www.takeourbtc.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://oneadfaucet.com/btc/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://ifaucet.info/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY#sthash.cwETeqOt.dpuf",
+"http://freebitcoinhome.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://faucetsfree.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://earnbitcoinonline.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://www.drawcoins.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://www.babyfreebtc.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://www.casinofaucetbitc.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://neobitcoinfaucet.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://freesatoshi.com.ua/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://bit-essence.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://www.freebtc.us/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://www.faucetgoldtime.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://kingofmachines.net/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://freebitcoins.online/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://printmoney.info/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://freecoins.info/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://faucet.jokertimes.co/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://yourfaucet.net/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://faucetix.ru/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://www.mrchocobit.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://claimfaucet.info/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://faucetcomic.info/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://rainingfreebitcoins.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://faucetsgalaxy.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://bluesatoshi.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://razercrypt.com/btc/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://xchange.cc/bitcoincran/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://btc4me.xyz/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://faucet.bitcoins43.com/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY ",
+"http://bitcoincoin.online/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://www.breaktheco.in/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://www.safebitco.in/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://theearner.co.in/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY",
+"http://www.get-free-bitco.in/?r=1Mx3pjzQDt7fE1Qp7tG2QwFT24RvBQtuUY"
+];
+var adr,i,x=0,c=s.length;
+function address() {
+adr=prompt('Enter your bitcoin address:');
+s=s.map(function(x){
+  return x.indexOf("a=") != -1 ? x + adr : x; 
+});
+}
+function next(){
+x+=1;
+if (x>c-1)
+  {
+  x=0;
+  }
+changeSrc();
+}
+function prev(){
+x-=1;
+if (x<=0)
+  {
+  x=c-1;
+  }
+changeSrc();
+}
+function jumpTo() {
+i=prompt("Enter a number to skip to that faucet.");
+x=0;
+while (i!=x) {
+  if (x>c-2) {
+  break;
+  }
+  x+=1;
+}
+changeSrc();
+}
+function newTab() {
+var win=window.open(document.getElementById("fm").src, '_blank');
+win.focus();
+}
+function changeSrc() {
+document.getElementById("fm").src=s[x];
+}
