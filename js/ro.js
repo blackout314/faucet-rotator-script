fde937de77188de0c8e6ead7a344f0912108371f
blackout314 <blackout_lair@yahoo.com>
2015-11-16 10:44:34 +0100

    add new dir

---
+++ js/ro.js
@@ -0,0 +1,43 @@
+
+var adr,
+  i,
+  x=0,
+  c=s.length;
+
+function next(){
+  x+=1;
+  if (x>c-1) {
+    x=0;
+  }
+  changeSrc();
+}
+
+function prev(){
+  x-=1;
+  if (x<=0) {
+    x=c-1;
+  }
+  changeSrc();
+}
+
+function jumpTo() {
+  i=prompt("Enter a number to skip to that faucet.");
+  x=0;
+  while (i!=x) {
+    if (x>c-2) {
+      break;
+    }
+    x+=1;
+  }
+  changeSrc();
+}
+
+function newTab() {
+  var win=window.open(document.getElementById("fm").src, '_blank');
+  win.focus();
+}
+
+function changeSrc() {
+  document.getElementById("fm").src=s[x];
+}
+
